7cd81de954a30e7cf4ef35014bc7b3ae5eecd2e6
EA - Rafael Lima Viana <lima_viana@educadventista.org>
2023-09-21 17:50:52 -0300

    menu-mobile

--- 2_MENU_RESPONSIVO/1_aula/src/components/Header.jsx
+++ 2_MENU_RESPONSIVO/1_aula/src/components/Header.jsx
@@ -1,16 +1,24 @@
 import { List, Hamburger } from "@phosphor-icons/react"
+import styles from "./Header.module.css"
 
 function Header() {
   return (
     <header>
         <div className='mobile'>
-            <div className="menu_logo">
+            <div className={styles.menu_logo}>
                 <button><List size={32}></List></button>
 
                 <Hamburger size={32} />
-
-                
             </div>
+
+            <div className={styles.opcoes}>
+                  <nav>
+                    <a href="">Home</a>
+                    <a href="">Ferramentas</a>
+                    <a href="">Objetivos</a>
+                    <a href="">Bibliografia</a>
+                  </nav>
+                </div>
         </div>
     </header>
   )
